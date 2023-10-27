51fccaea7cd2e57f8c4d45ebbae85bdf0a472e0e
ReneChungara <renechungara03@gmail.com>
2023-10-26 20:32:38 -0400

    Modificacion vista comentario

--- resources/views/VistaWelcome/contacto.blade.php
+++ resources/views/VistaWelcome/contacto.blade.php
@@ -6,7 +6,7 @@
     </div>
     <div class="container px-5 py-24 mx-auto flex">
         <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Comentario</h2>
+            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Envianos un mensaje</h2>
             <p class="leading-relaxed mb-5 text-gray-600"></p>
             <div class="relative mb-4">
             <label for="email" class="leading-7 text-sm text-gray-600">Correo Electronico</label>
